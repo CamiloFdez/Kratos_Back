1bf2e6c7833ab98fbd895b05e9eb005fa87a1c1e
Andres Jacobo <andres.sepulveda-s@mail.escuelaing.edu.co>
2025-02-26 15:09:42 -0500

    Agrego configuración inicial con spring boot

---
+++ src/main/java/eci/escuelaing/edu/co/ProyectoCvdsApplication.java
@@ -0,0 +1,13 @@
+package eci.escuelaing.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCvdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCvdsApplication.class, args);
+	}
+
+}
