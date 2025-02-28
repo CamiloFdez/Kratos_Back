dbb7f6c5f675baaefff9270f67c264c5f0ffa6bb
Sebastian-villarraga <157900873+Sebastian-villarraga@users.noreply.github.com>
2025-02-27 22:11:11 -0500

    cree tests

--- src/main/java/eci/escuelaing/edu/co/webSocket/WebSocketConfig.java
+++ src/main/java/eci/escuelaing/edu/co/webSocket/WebSocketConfig.java
@@ -1,5 +1,15 @@
 package eci.escuelaing.edu.co.webSocket;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new HorarioWebSocketHandler(), "/horarios-ws").setAllowedOrigins("*");
+    }
 }
---
+++ src/test/java/eci/escuelaing/edu/co/horarios/HorarioIntegrationTest.java
@@ -0,0 +1,5 @@
+package eci.escuelaing.edu.co.horarios;
+
+public class HorarioIntegrationTest {
+
+}
