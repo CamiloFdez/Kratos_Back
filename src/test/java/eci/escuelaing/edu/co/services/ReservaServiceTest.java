a79b1d0c1523d642d3be064b44d4f7b1f4d120ea
Roger <rogeralexander902@gmailcom>
2025-03-12 16:19:16 -0500

    correcion ReservaServiceTest

--- src/test/java/eci/escuelaing/edu/co/services/ReservaServiceTest.java
+++ src/test/java/eci/escuelaing/edu/co/services/ReservaServiceTest.java
@@ -29,29 +29,25 @@ public class ReservaServiceTest {
 
     @Test
     void shouldReturnAllReservas() {
-        // Arrange
+
         Reserva reserva1 = new Reserva("1", "1", "Lab1", null, "Practica 1", 1);
         Reserva reserva2 = new Reserva("2", "2", "Lab2", null, "Practica 2", 5);
         when(reservaRepository.findAll()).thenReturn(Arrays.asList(reserva1, reserva2));
 
-        // Act
         var reservas = reservaService.ObtainAllReservas();
 
-        // Assert
         assertEquals(2, reservas.size());
         verify(reservaRepository, times(1)).findAll();
     }
 
     @Test
     void shouldReturnReservaById() {
-        // Arrange
+
         Reserva reserva = new Reserva("1", "1", "Lab1", null, "Practica 1", 1);
         when(reservaRepository.findByFechaHora(null)).thenReturn(Optional.of(reserva));
 
-        // Act
         var result = reservaService.ObtainReservaById(null);
 
-        // Assert
         assertTrue(result.isPresent());
         assertEquals("1", result.get().getUsuarioId());
         verify(reservaRepository, times(1)).findByFechaHora(null);
@@ -59,15 +55,13 @@ public class ReservaServiceTest {
 
     @Test
     void shouldCreateReserva() {
-        // Arrange
+
         Reserva reserva = new Reserva("1", "1", "Lab1", null, "Practica 1", 1);
         when(reservaRepository.findByFechaHora(null)).thenReturn(Optional.empty());
         when(reservaRepository.save(reserva)).thenReturn(reserva);
 
-        // Act
         var result = reservaService.CreateReserva(reserva);
 
-        // Assert
         assertEquals("1", result.getId());
         verify(reservaRepository, times(1)).findByFechaHora(null);
         verify(reservaRepository, times(1)).save(reserva);
@@ -75,16 +69,12 @@ public class ReservaServiceTest {
 
     @Test
     void shouldUpdateReserva() {
-        // Arrange
         Reserva reserva = new Reserva("1", "1", "Lab1", null, "Practica 1",1);
         Reserva reservaActualizada = new Reserva("1", "2", "Lab2", null, "Practica 2", 5);
         when(reservaRepository.findByFechaHora(null)).thenReturn(Optional.of(reserva));
         when(reservaRepository.save(reserva)).thenReturn(reserva);
 
-        // Act
         var result = reservaService.UpdateReserva(null, reservaActualizada);
-
-        // Assert
         assertEquals("2", result.getUsuarioId());
         assertEquals("Lab2", result.getLaboratorio());
         assertEquals("Practica 2", result.getProposito());
@@ -98,20 +88,14 @@ public class ReservaServiceTest {
         Reserva reservaActualizada = new Reserva("1", "2", "Lab2", null, "Practica 2", 5);
         when(reservaRepository.findByFechaHora(null)).thenReturn(Optional.empty());
 
-        // Act & Assert
         assertThrows(IllegalArgumentException.class, () -> reservaService.UpdateReserva(null, reservaActualizada));
         verify(reservaRepository, times(1)).findByFechaHora(null);
     }
 
     @Test
     void shouldDeleteReserva() {
-        // Arrange
         Reserva reserva = new Reserva("1", "1", "Lab1", null, "Practica 1", 1);
-
-        // Act
         reservaService.DeleteReserva(null);
-
-        // Assert
         verify(reservaRepository, times(1)).deleteByFechaHora(null);
     }
 
